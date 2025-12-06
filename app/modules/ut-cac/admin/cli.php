90ce2cefbfbff117031d62a51e03224d742791bc
usualtool <292951110@qq.com>
2025-12-06 18:37:09 +0800

    update 251206

--- app/modules/ut-cac/admin/cli.php
+++ app/modules/ut-cac/admin/cli.php
@@ -1,8 +1,9 @@
 <?php
-    $o=str_replace("%26","",str_replace("%7C","",str_replace("&","",str_replace("|","",$_POST["o"]))));
-    if(substr($o,0,2)=="cd" || substr($o,0,3)=="php" || substr($o,0,5)=="nohup" ||substr($o,0,8)=="composer"):
-        $results = shell_exec($o);
-        echo"<p>[".$_SESSION['admin']."@localhost ~]#".$o." run complete.</p><p>".$results."</p>";
-    else:
-        echo"<p>[".$_SESSION['admin']."@localhost ~]#Command not supported.</p>";
-    endif;
+use library\UsualToolCli\UTCli;
+$o=str_replace("%26","",str_replace("%7C","",str_replace("&","",str_replace("|","",$_POST["o"]))));
+if(substr($o,0,2)=="cd" || substr($o,0,3)=="php" || substr($o,0,5)=="nohup" ||substr($o,0,8)=="composer"):
+    $results = UTCli::Execute($o);
+    echo$results;
+else:
+    echo"Command not supported.";
+endif;
