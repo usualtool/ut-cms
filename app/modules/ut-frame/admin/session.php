30090258745df3539a2957dcc3fa19eeba352f26
usualtool <292951110@qq.com>
2026-04-25 00:16:11 +0800

    update 260425

--- app/modules/ut-frame/admin/session.php
+++ app/modules/ut-frame/admin/session.php
@@ -2,6 +2,45 @@
 use library\UsualToolInc\UTInc;
 use library\UsualToolData\UTData;
 if(isset($_SESSION['admin'])&&isset($_SESSION['admin_id'])&&!empty($_SESSION['admin'])&&!empty($_SESSION['admin_id'])):
+		/**
+		 * 获取版本号并载入应用部分设置
+		 */
+		$framework=file_get_contents(UTF_ROOT."/.version.ini");
+		$version=substr($frame_version,0,5);
+		$version_time=substr($frame_version,-6);
+		$app->Runin(
+				array("version","version_time","update","develop","lock"),
+				array($version,$version_time,$config["UPDATEURL"],$config["DEVELOP_OPEN"],$config["LOCKSCREEN"])
+		);
+		/**
+		 * 接收官方消息
+		 */
+		$webmsg=UTF_ROOT."/log/usualtool.log";
+		$msgdata=file_exists($webmsg) ? json_decode(file_get_contents($webmsg),true) : null;
+		if(!$msgdata || time()-strtotime($msgdata["time"])>86400):
+				$msg=UTInc::Auth($config["UTCODE"],$config["UTFURL"],"message");
+				file_put_contents($webmsg,json_encode(["time"=>date("Y-m-d"),"message"=>$msg],JSON_UNESCAPED_UNICODE));
+		else:
+				$app->Runin("message", explode("|", $msgdata["message"]));
+		endif;
+		/**
+		 * 底层模块
+		 * 除开UT-FRAME公共模块
+		 */
+		$app->Runin("modules",UTData::QueryData("cms_module","","bid=3 and mid<>'".$config["DEFAULT_MOD"]."'","","")["querydata"]);
+		/**
+		 * 自定义模块
+		 */
+		$app->Runin("custmods",UTData::QueryData("cms_module","","bid<>3")["querydata"]);
+		/**
+		 * 当前运行模块及栏目
+		 * befoitem前端地址集，backtem后端栏目集
+		 */
+		$thismod=UTData::QueryData("cms_module","","mid='$m'","","")["querydata"][0];
+		$app->Runin(
+				array("title","befoitem","backitem"),
+				array($thismod["modname"],$thismod["befoitem"],$thismod["backitem"])
+		);
     $admin=$_SESSION['admin'];
     $admin_id=$_SESSION['admin_id'];
     $admin_roleid=$_SESSION['admin_roleid'];
