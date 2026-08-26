7abbb2413a02e4172c655f06c8701f70321777a9
usualtool <292951110@qq.com>
2026-08-26 18:31:15 +0800

    update 260826

--- app/modules/ut-template/admin/template_view.php
+++ app/modules/ut-template/admin/template_view.php
@@ -2,7 +2,9 @@
 use library\UsualToolInc\UTInc;
 use library\UsualToolRoute\UTRoute;
 $do=$_GET["do"];
-$d=UTInc::SqlCheck(str_replace("|","/",str_replace("%7C","/",str_replace("_cms",".cms",str_replace("..","",$_GET["d"])))));
+$d=empty($_POST["d"]) ? $_GET["d"] : $_POST["d"];
+$d=urldecode($d);
+$d=UTInc::SqlCheck(str_replace("|","/",str_replace("%7C","/",str_replace("_cms",".cms",str_replace("..","",$d)))));
 $html=file_get_contents(APP_ROOT."/modules/".$d);
 $app->Runin(array("d","file","html"),array($d,APP_ROOT."/modules/".$d,htmlspecialchars($html)));
 /**
@@ -10,10 +12,10 @@ $app->Runin(array("d","file","html"),array($d,APP_ROOT."/modules/".$d,htmlspecia
  */
 $app->Open("template_view.cms");
 if($do=="save"){
-    $file=str_replace("..","",$_POST["filename"]);
-    $filex=str_replace("/","|",str_replace(".cms","_cms",$_POST["filename"]));
+    $file=str_replace("..","",$_POST["d"]);
+    $filex=str_replace("/","|",str_replace(".cms","_cms",$_POST["d"]));
     $file_path=APP_ROOT."/modules/".$file;
     $content=htmlspecialchars_decode($_POST["content"]);
     file_put_contents($file_path,$content);
-    UTInc::GoUrl("?m=ut-template&p=template_view&d=".$filex,'更新模板成功!');
+    UTInc::GoUrl(UTRoute::Link("ut-template","template_view","d=".$filex),'更新模板成功!');
 }
