77713227a7e92ec658486c930d804477b614d3aa
usualtool <292951110@qq.com>
2026-09-06 12:09:39 +0800

    update 260906

--- app/modules/ut-api/front/data-robot.php
+++ app/modules/ut-api/front/data-robot.php
@@ -2,12 +2,13 @@
 //需启用usualtool/ut-ai扩展
 use usualtool\Ai\Ai;
 require'data-verify.php';
-if($_SERVER['REQUEST_METHOD']==='POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $input = file_get_contents('php://input');
     $data = json_decode($input, true);
     $messages = $data['messages'] ?? [];
     $model = $data['model'] ?? 'GLM-4.7-FlashX';
     $stream = $data['stream'] ?? false;
+    $sessionid = $data['sessionid'] ?? '';
     $ai = new Ai();
-    echo $ai->Chat($messages, $model, $stream);
+    echo $ai->Chat($messages, $model, $stream, $sessionid);
 }
